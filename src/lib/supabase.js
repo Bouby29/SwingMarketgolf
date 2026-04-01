1668a676d0d6914b68132e2de4fa8392265686a9
Alexandre <alexandre.dnl@orange.fr>
2026-04-01 22:09:00 +0200

    fix: clés Supabase correctes

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ahhowdesruijqgcpemhu.supabase.co'
-const supabaseKey = 'sb_publishable_JbvmLE-PYA3g5--KVXvnLQ_E6XaHxUp'
+const supabaseUrl = 'https://pnhiuifejnnklbfpjmdr.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBuaGl1aWZlam5ua2xiZnBqbWRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2OTA3ODUsImV4cCI6MjA4OTI2Njc4NX0.PSnt0z41OiiUvoJgp3S5WO5g8Mhz-GORZbExlo_SXho'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 export { entities, auth } from "./api";
