8d8d1f4a8cf1c9f4dd8ccb1c31ba9274d380071e
Alexandre <alexandre.dnl@orange.fr>
2026-04-05 22:10:43 +0200

    fix: i18n placeholder Navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -104,7 +104,7 @@ export default function Navbar() {
           <form onSubmit={handleSearch} className="hidden md:flex flex-1 max-w-xl mx-4">
             <div className="relative w-full">
               <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" />
-              <input type="text" placeholder={t("common.search") + "..."} value={searchQuery}
+              <input type="text" placeholder={`${t("common.search")}...`} value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
                 className="w-full pl-10 pr-4 py-2.5 bg-gray-50 border border-gray-200 rounded-full text-sm focus:outline-none focus:border-[#2E7D32] focus:ring-1 focus:ring-[#2E7D32] transition-all" />
             </div>
